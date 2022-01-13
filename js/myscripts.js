10537ce08f0d164200b7d54d731149e4b9d01bf9
DanieleFic <reb_ficus22@hotmail.it>
2022-01-13 14:55:13 +0100

    ti prego vue semplificaci la vita

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,2 +1,12 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 //Bonus:Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+let app = new Vue({
+    el: '#container',
+    data: {
+        messaggio:"Ciao",
+        foto:"https://venetostatofederale.files.wordpress.com/2014/12/ciao.jpg",
+    }
+    
+})
+
