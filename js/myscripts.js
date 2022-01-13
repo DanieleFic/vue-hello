9573f71b66700396b524b14bc5a5ea6ed2c20d7f
DanieleFic <reb_ficus22@hotmail.it>
2022-01-13 14:57:21 +0100

    bonus

--- js/myscripts.js
+++ js/myscripts.js
@@ -4,8 +4,8 @@
 let app = new Vue({
     el: '#container',
     data: {
-        messaggio:"Ciao",
-        foto:"https://venetostatofederale.files.wordpress.com/2014/12/ciao.jpg",
+        messaggio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cum quaerat illum praesentium. Iusto tempore voluptas repellendus sapiente, eum sit ea dolorem id aliquid, autem non cumque provident delectus, eos voluptate? Aspernatur, labore, facilis quae nulla suscipit commodi, rerum asperiores perspiciatis ipsa doloremque molestias nisi nesciunt quisquam quia aliquam libero?",
+        foto:"https://media.istockphoto.com/vectors/a-colorful-trendy-card-design-vector-illustration-vector-id1193323373?k=20&m=1193323373&s=612x612&w=0&h=2FmXahDQKYY2zPpdrgzh5QxWqPu3RTCoduKebKNfQRg=",
     }
     
 })
